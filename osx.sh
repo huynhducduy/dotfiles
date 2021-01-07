58a770d2123913e0838f2a5d253af13b180e34a1
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2021-01-07 19:26:26 +0700

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -71,9 +71,6 @@ defaults write com.apple.LaunchServices LSQuarantine -bool false
 # Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
 defaults write NSGlobalDomain AppleFontSmoothing -int 1
 
-# Enable HiDPI display modes (requires restart)
-sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
-
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
