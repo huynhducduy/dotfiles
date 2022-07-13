9d0b46cd4b900b13a05cb35c866eb649c64ae919
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2022-07-14 03:23:38 +0700

    Switch to FastForward
    https://chrome.google.com/webstore/detail/fastforward/icallnadddjmdinamnolclfjanhfoafe/related

--- .chrome-extensions/Universal.Bypass.for.Chromium-based.browsers/html/i18n.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll("[data-message]").forEach(e=>e.textContent=brws.i18n.getMessage(e.getAttribute("data-message")))
-const titleElm=document.querySelector("title"),title=titleElm.textContent
--- .chrome-extensions/Universal.Bypass.for.Chromium-based.browsers/html/popup.js
+++
@@ -1,2 +0,0 @@
-brws.runtime.sendMessage({type: "open-tab", url: brws.runtime.getURL("/html/options.html")})
-window.close()
